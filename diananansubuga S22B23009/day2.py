5b3fbb217961b3536e69c26d46bd8f7b30608bed
Diananansubuga <117372394+Diananansubuga@users.noreply.github.com>
2023-09-20 09:16:23 +0300

    Add files via upload

---
+++ diananansubuga S22B23009/day2.py
@@ -0,0 +1,6 @@
+# create a pyhton programme that shows which version of pyhton you are using
+import sys
+print("Python version")
+print (sys.version)
+print("Version info.")
+print (sys.version_info)
