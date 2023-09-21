95e14366dad8bb28877ce4ee0de63a59d73e5133
Diananansubuga <117372394+Diananansubuga@users.noreply.github.com>
2023-09-21 14:08:18 +0300

    Update and rename day2.py to day1.py

--- diananansubuga S22B23009/day2.py
+++
@@ -1,6 +0,0 @@
-# create a pyhton programme that shows which version of pyhton you are using
-import sys
-print("Python version")
-print (sys.version)
-print("Version info.")
-print (sys.version_info)
