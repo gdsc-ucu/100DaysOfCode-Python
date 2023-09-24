81031e27e1cdee8cd5cd4c201be1697121152bdf
Okidi Norbert <145354274+OkidNorbert@users.noreply.github.com>
2023-09-24 20:06:16 +0300

    Updated day5.py

--- Okidi Norbert B24281/day5.py
+++ Okidi Norbert B24281/day5.py
@@ -5,9 +5,9 @@ my_list = ["coding", "hacking", "cracking"]
 my_tuple = ("python", "java", "c++")
 my_dic = {"OS type:""Athena OS", "Best lang:""Python"}
 
-print(my_int, type(my_int))
-print(my_float, type(my_float))
-print(my_string, type(my_string))
-print(my_list, type(my_list))
-print(my_tuple, type(my_tuple))
-print(my_dic, type(my_dic))
+print("my_int:", my_int, "Type:", type(my_int))
+print("my_float:", my_float, "Type:", type(my_float))
+print("my_string:", my_string, "Type:", type(my_string))
+print("my_list:", my_list, "Type:", type(my_list))
+print("my_tuple:", my_tuple, "Type:", type(my_tuple))
+print("my_dic:", my_dic, "Type:", type(my_dic))
