c53f9a80c64b12d0969a9234ad9ff0c77932ca07
Peter Kure <peterkure256@gmail.com>
2023-09-24 22:02:09 +0300

    Day 5: variables and data types

---
+++ Kure Peter A96404/Day_5/Day5.py
@@ -0,0 +1,16 @@
+#Variables and their types
+my_int = 1
+my_float = float(12)
+my_string='string'
+my_list=[1,2,3,4,5]
+my_tuple=()
+my_dict={}
+
+# printing the data types
+print("The data types are: ")
+print(type(my_int))
+print(type(my_float))
+print(type(my_string))
+print(type(my_list))
+print(type(my_tuple))
+print(type(my_dict))
