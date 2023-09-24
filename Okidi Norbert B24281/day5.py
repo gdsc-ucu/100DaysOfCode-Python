23dab95fdae39904058c0c9850ff371eb7ba05ee
Okidi Norbert <145354274+OkidNorbert@users.noreply.github.com>
2023-09-24 19:58:51 +0300

    Add files via upload

---
+++ Okidi Norbert B24281/day5.py
@@ -0,0 +1,13 @@
+my_int = 6
+my_float = 8.5
+my_string = "Norbert is the best"
+my_list = ["coding", "hacking", "cracking"]
+my_tuple = ("python", "java", "c++")
+my_dic = {"OS type:""Athena OS", "Best lang:""Python"}
+
+print(my_int, type(my_int))
+print(my_float, type(my_float))
+print(my_string, type(my_string))
+print(my_list, type(my_list))
+print(my_tuple, type(my_tuple))
+print(my_dic, type(my_dic))
