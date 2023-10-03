fd65bbeed20aa166135949a32fcd98d3092d1a0b
Okidi Norbert <145354274+OkidNorbert@users.noreply.github.com>
2023-10-03 23:06:41 +0300

    Create oct-day3.py
    day 3 of my coding

---
+++ Okidi Norbert B24281/oct-day3.py
@@ -0,0 +1,18 @@
+def multiply(): #in the first line "def" declares its a funtion called multiply
+    numbers = 4*9*5*6*4 #the second statement gets the product of the numbers 
+    print("The product of the numbers is",numbers)  #the 3rd statement displays the result of variable "x" in the terminal
+    #statement 2 and 3 belong to one block of code with the multiply() function
+
+multiply()
+
+def student_grades():
+    norbert = 90
+    tetricia = 80
+    zahra = 85
+    print("Norbert:{}%".format(norbert))
+    print("Zahra:{}%".format(zahra))
+    print("Tetricia:{}%".format(tetricia))
+
+
+
+student_grades()
