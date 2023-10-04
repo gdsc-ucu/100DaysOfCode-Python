0b4711172a5f92de7cbc4f1091c58da1aaac83c0
ATWIINETIRZAH <tirzahatwiine5@gmail.com>
2023-10-04 21:51:24 +0300

    created function

---
+++ day3.py
@@ -0,0 +1,8 @@
+def Bio():
+    name=input("Please enter your name: ")
+    age=int(input("please enter your age: "))
+    hobby=input("enter your hobby: ")
+    print(f"Your name is {name},You are {age}years old and you like {hobby}")
+
+print("Hello!!!")
+Bio()
