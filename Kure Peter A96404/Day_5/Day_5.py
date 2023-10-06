de9bcc29667bdefd3edc80fb33b60374db08f2bc
Peter Kure <peterkure256@gmail.com>
2023-10-06 09:20:25 +0300

    Day 5: Variables and their types

---
+++ Kure Peter A96404/Day_5/Day_5.py
@@ -0,0 +1,15 @@
+# Declaring the variables
+my_int=int()
+my_float=float()
+my_string=str()
+my_list=list()
+my_tuple=tuple()
+my_dict=dict()
+
+# Printing the variables and their types
+print(f"The variable my_int and its type: {type(my_int)}")
+print(f"The variable my_float and its type: {type(my_float)}")
+print(f"The variable my_string and its type: {type(my_string)}")
+print(f"The variable my_list and its type: {type(my_list)}")
+print(f"The variable my_tuple and its type: {type(my_tuple)}")
+print(f"The variable my_dict and its type: {type(my_dict)}")
