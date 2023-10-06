f8c9de46385493cc6ef173ea1291663848a24c24
EzamamtiRonaldAustine <145444276+EzamamtiRonaldAustine@users.noreply.github.com>
2023-10-06 18:59:41 +0300

    Update Repo

--- 4 python/Day4.py
+++
@@ -1,6 +0,0 @@
-student_name = "Alice"
-grades = 85,90,88
-average_grade = (85+90+88)/3
-print(student_name)
-print(grades)
-print(average_grade)
--- 6 python/Day6.py
+++
@@ -1,5 +0,0 @@
-name = input("Enter your name: ")
-age = int(input("Enter your age: "))  
-float_number = float(input("Enter a floating-point number: "))
-message = f"Hello, {name}! You are {age} years old, and you entered the number {float_number:.2f}."
-print(message)
