6593aaec3f03a24930ec740961e534712a4a3218
EzamamtiRonaldAustine <145444276+EzamamtiRonaldAustine@users.noreply.github.com>
2023-10-06 20:26:35 +0300

    Add files via upload

---
+++ Ezamamti Ronald Austine B24252/Python 1/Python 5/Day5.py
@@ -0,0 +1,13 @@
+my_int = 100
+my_float = 6.19
+my_string = 'James Bond' 
+my_list = ["leo","Steve","Ronald"]
+my_tuple = (4,7,6,1)
+my_dict = {"name":"Max","country":"France","hobby":"eating"}
+
+print(my_int)
+print(my_float)
+print(my_string)
+print(my_list)
+print(my_tuple)
+print(my_dict)
