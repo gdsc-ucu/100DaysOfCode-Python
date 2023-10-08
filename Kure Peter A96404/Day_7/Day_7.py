6af6df6ad3b5a2ce393c94f38f673f8c9ee257f3
Peter Kure <peterkure256@gmail.com>
2023-10-08 10:49:08 +0300

    Day 7: Shopping Cart with control structures

---
+++ Kure Peter A96404/Day_7/Day_7.py
@@ -0,0 +1,16 @@
+# Control Structures
+
+def shoppingCart():
+    total_cost = int(input("What's the total cost of your shopping cart?: "))
+    if total_cost >= 50:
+        discount = 0.1 * total_cost
+        discountedTotalCost = total_cost - discount
+        print(
+            f"The original total cost is ${total_cost}"
+        )
+        print(f"The discounted total cost is ${discountedTotalCost}")
+    else:
+        print("There's no discount applied")
+
+
+shoppingCart()
