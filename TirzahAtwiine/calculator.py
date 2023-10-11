c565a3ebc3847c3fc6e56b9c1d291c62ab5c8f00
ATWIINETIRZAH <tirzahatwiine5@gmail.com>
2023-10-11 15:34:58 +0300

    created calculator module

---
+++ TirzahAtwiine/calculator.py
@@ -0,0 +1,22 @@
+
+
+def Add(value_1,value_2):
+    addition = value_1 + value_2
+    print(f"{value_1} + {value_2} is: {addition}")
+    # return add
+
+def Subtract(value_1,value_2):
+    subtract=value_1 - value_2
+    print(f"{value_1} - {value_2} is: {subtract}")
+    # return subtract
+
+def Divide(value_1,value_2):
+    divide=value_1 / value_2
+    print(f"{value_1} / {value_2} is: {divide}")
+    # return divide
+
+def Multiply(value_1,value_2):
+    multiply=value_1 * value_2
+    print(f"{value_1} * {value_2} is: {multiply}")
+    # return multiply
+
