6d498fa06f3873840ab50521527f249425db9871
EzamamtiRonaldAustine <145444276+EzamamtiRonaldAustine@users.noreply.github.com>
2023-10-11 20:24:17 +0300

    Add files via upload

---
+++ Python 10/calculator.py
@@ -0,0 +1,22 @@
+def add_numbers(a,b):
+    return a+b
+def subtract_numbers(a,b):
+    return a-b
+def multiply_numbers(a,b):
+    return a*b
+def divide_numbers(a,b):
+    if b == 0:
+         return "error : Division by zero refused."
+    return a/b
+
+
+
+
+
+
+
+
+
+
+
+
