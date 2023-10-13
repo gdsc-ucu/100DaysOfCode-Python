25ad4520e5c37311597077e36f1d6c564b1ac9d9
Peter Kure <peterkure256@gmail.com>
2023-10-14 01:14:58 +0300

    Revert "Day 14: Module for Area of a circle"
    This reverts commit 1a1ba3e86ea3df081bad5042defcf2d25e25a9e0.

--- Kure Peter A96404/Day_13/AreaModule.py
+++
@@ -1,4 +0,0 @@
-def area():
-    r = int(input("Enter radius here: "))
-    areaOfcircle = 3.14*r*r
-    print(f"The area of the circle is {areaOfcircle} sq.units")
--- Kure Peter A96404/Day_13/Day_13.py
+++
@@ -1,5 +0,0 @@
-# Importing the module
-import AreaModule
-
-# Area of circle based on radius as input
-AreaModule.area()
