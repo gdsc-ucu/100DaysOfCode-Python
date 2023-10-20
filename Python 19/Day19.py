41b8c205f4875278ad13dcc1589cc987002fe615
EzamamtiRonaldAustine <145444276+EzamamtiRonaldAustine@users.noreply.github.com>
2023-10-20 14:24:52 +0300

    Update Day19.py

--- Python 19/Day19.py
+++ Python 19/Day19.py
@@ -2,4 +2,4 @@ file_name = "To do list.txt"
 User_input = input("Enter your to do list:")
 with open(file_name, 'w') as file:
     file.write(User_input)
-    print("Notes saved to",)
+    print("Notes saved to",file_name)
