2c5faaf0000858fd3871a1ae236d9d2d64979ab7
Peter Kure <peterkure256@gmail.com>
2023-10-29 19:10:58 +0300

    Day 28: Program that prints sqrt of date

---
+++ Kure Peter A96404/Day_28/Day_28.py
@@ -0,0 +1,14 @@
+# Combining libraries 
+import math
+import datetime
+
+# current day of the month
+currentDate = datetime.date.today()
+
+# Square root of the day
+dayOfMonth = currentDate.day
+squareRoot = math.sqrt(dayOfMonth)
+
+# Printing the output
+print(f"THe square root of the days day of the month {dayOfMonth} is {squareRoot}")
+
