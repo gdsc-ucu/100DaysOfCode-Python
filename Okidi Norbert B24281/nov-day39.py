0b6c930f091b274051296c61fe8644de73b08aab
Okidi Norbert <145354274+OkidNorbert@users.noreply.github.com>
2023-11-11 12:55:30 +0300

    updated code

--- Okidi Norbert B24281/nov-day39.py
+++ Okidi Norbert B24281/nov-day39.py
@@ -5,12 +5,12 @@ class Shape:
         pass
 
 class Rectangle(Shape):
-    def __init__(self, width, height):
+    def __init__(self, length, width):
+        self.length = length
         self.width = width
-        self.height = height
 
     def area(self):
-        return self.width * self.height
+        return self.length * self.width
 
 class Circle(Shape):
     def __init__(self,radius):
