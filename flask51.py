b4b1b805ec795c0d373d9e7f6c5657c05107b524
EzamamtiRonaldAustine <145444276+EzamamtiRonaldAustine@users.noreply.github.com>
2023-11-22 12:51:54 +0300

    Update flask51.py

--- flask51.py
+++ flask51.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
 
-@app.route('/signup', methods=['GET', 'POST'])
+@app.route('/', methods=['GET', 'POST'])
 def signup():
     if request.method == 'POST':
         username = request.form['username']
