015b8e360d2ff08e5050a46278664eba5f4d4384
Peter Kure <peterkure256@gmail.com>
2023-11-22 20:50:40 +0300

    Day 52: Forms and User Input(Continued)

---
+++ Kure Peter A96404/Day_52/djangoformsproject/djangoformsapp/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+
+from djangoformsapp.forms import SignupForm
+# Create your views here.
+
+def signup(request):
+    if request.method == 'POST':
+        form = SignupForm(request.POST)
+        if form.is_valid():
+            # Process the data, e.g., save it to a database, etc.
+            return render(request, 'signup_success.html', {
+                'username': form.cleaned_data['username'],
+                'email': form.cleaned_data['email'],
+                'age': form.cleaned_data['age']
+            })
+    else:
+        form = SignupForm()
+    return render(request, 'signup.html', {'form': form})
