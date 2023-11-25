fbc03d687f5808b59ea52e1e3b38edb68fc0207d
Okidi Norbert <145354274+OkidNorbert@users.noreply.github.com>
2023-11-25 11:41:47 +0300

    updated

--- Okidi Norbert B24281/nov-day43.py
+++ Okidi Norbert B24281/nov-day43.py
@@ -1,16 +1,15 @@
 from flask import Flask
-
+from django.shortcuts import HttpResponse
 app = Flask(__name__)
 
 @app.route('/')
 
 def hello_world():
     return 'Hello World'
-
+def home(request):
+    return HttpResponse('Hello, world')
 if __name__ == '__main__':
     app.run(debug=True)
 
-from django.shortcuts import HttpResponse
 
-def home(request):
-    return HttpResponse('Hello, world')
+
