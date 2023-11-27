f2840419b0802ba2b6ebfa83fa42ce406c527932
Okidi Norbert <145354274+OkidNorbert@users.noreply.github.com>
2023-11-27 14:00:21 +0300

    day 57

--- Okidi Norbert B24281/day57/flask/app.py
+++ Okidi Norbert B24281/day57/flask/app.py
@@ -1,7 +1,11 @@
 from flask import Flask, render_template, request
-
+from flask_caching import Cache
+from flask_compress import Compress
 
 app = Flask(__name__)
+
+cache = Cache(app, config={'CACHE_TYPE': 'simple'})
+Compress(app)
 
 
 @app.route('/')
@@ -14,10 +18,10 @@ def sign():
     username = request.form['username']
     email = request.form['email']
     if not username or not email:
-        return "Failure Both username and email required."
+        return "Failure: Both username and email required."
 
     return render_template("signup_successful.html")
 
 
-if __name__ == 'main':
+if __name__ == '__main__':
     app.run(debug=True)
