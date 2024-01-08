3cc35877072bf0c447beafe8d5cae26447170d60
Okidi Norbert <145354274+OkidNorbert@users.noreply.github.com>
2024-01-08 13:25:05 +0000

    day99

--- Okidi Norbert B24281/day92-100/venv/Lib/site-packages/eventlet/hubs/pyevent.py
+++
@@ -1,4 +0,0 @@
-raise ImportError(
-    "Eventlet pyevent hub was removed because it was not maintained."
-    " Try version 0.22.1 or older. Sorry for the inconvenience."
-)
--- Okidi Norbert B24281/day92-100/venv/Scripts/activate.bat
+++ Okidi Norbert B24281/day92-100/venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\BALL IS LIFE\Documents\GitHub\100DaysOfCode-Python\Okidi Norbert B24281\day92-100\venv
+set VIRTUAL_ENV=C:\Users\vicent\Documents\GitHub\100DaysOfCode-Python\Okidi Norbert B24281\day92-100\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
